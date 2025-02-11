2df2fc2ee210ac12c4fdfcc619310d648d94fc12
Davitrons <davitrons@gmail.com>
2025-02-11 18:24:40 +0100

    Ejercicio 1: Suma Condicional de Números

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const array = [5, 12, 18, 7, 21, 30];
 const btn1 = document.getElementById("btn1").addEventListener('click', ()=>{
-
+    let sum = 0;
+    let resultado = "";
+    array.forEach(el => {
+        if (el > 10 && el % 3 === 0) {
+          resultado += ` ${el} `;
+          sum += el;  
+        }
+    });
+    console.log(`El resultado es : ${sum} // [${resultado}]`);
 });
