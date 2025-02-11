067fd117489231948039e9da1a2e86a5481aafcf
Davitrons <davitrons@gmail.com>
2025-02-11 19:06:11 +0100

    Ejercicio 4: Contar Tipos de Datos en un Array

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const array = [5, 12, 18, 7, 21, 30];
-const btn1 = document.getElementById("btn1").addEventListener('click', ()=>{
-    let sum = 0;
-    let resultado = "";
-    array.forEach(el => {
-        if (el > 10 && el % 3 === 0) {
-          resultado += ` ${el} `;
-          sum += el;  
-        }
-    });
-    console.log(`El resultado es : ${sum} // [${resultado}]`);
+const btn1 = document.getElementById("btn1").addEventListener("click", () => {
+  let sum = 0;
+  let resultado = "";
+  array.forEach((el) => {
+    if (el > 10 && el % 3 === 0) {
+      resultado += ` ${el} `;
+      sum += el;
+    }
+  });
+  console.log(`El resultado es : ${sum} // [${resultado}]`);
 });
 
 /* const obj = { a: "uno", b: "dos", c: "tres" };
@@ -20,3 +20,41 @@ const btn2 = document.getElementById("btn2").addEventListener('click', ()=>{
     }
     console.log(invert);
 }); */
+
+const arrayVariado = [1, "hola", true, 42, false, "mundo", null, undefined];
+const btn4 = document.getElementById("btn4").addEventListener("click", () => {
+  let obj={}  
+  let contnum = 0,
+    contstring = 0,
+    contbool = 0,
+    contobj = 0,
+    contundefined = 0;
+  arrayVariado.forEach((el) => {
+    switch (typeof el) {
+      case "number":
+        contnum++;
+        break;
+
+      case "string":
+        contstring++;
+        break;
+
+      case "boolean":
+        contbool++;
+        break;
+
+      case "object":
+        contobj++;
+        break;
+
+      case "undefined":
+        contundefined++;
+        break;
+      default:
+        console.log("Error al leer el tipo de dato");
+        break;
+    }
+  });
+
+  console.log(obj = {number : contnum, string:contstring, boolean:contbool, objetc:contobj, undefined:contundefined});
+});
